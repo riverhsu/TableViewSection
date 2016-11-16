5ef3be9eba2cc12f40d852457f0701805e57a89e
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-11-16 12:00:33 +0800

    step 2: create section & items for table cells step 3: set up parameters for sections and cells step 4: close project

--- TableViewSection/ViewController.swift
+++ TableViewSection/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var tableCells: UITableView!
+    let secTitle = ["Sec1","Sec2","Sec3"]
+    let details: [[String]] = [["Iron Man", "Spiderman"],["Hawk"], ["Thor"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableCells.delegate = self
+        tableCells.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.secTitle.count
+    }
+    
+     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.secTitle[section]
+    }
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.details[section].count
+    }
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.details[indexPath.section][indexPath.row]
+        return cell
+    }
 }
 
